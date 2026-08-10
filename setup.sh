36930a814856394baabae7da73a31455888ccf78
Ayato Matsui <ayatomatsui010@gmail.com>
2026-08-10 05:14:39 +0000

    changed setup and asset

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t mlforge .
+docker build --no-cache -t mlforge:latest .
 docker run --rm -p 18080:8080 --env-file .env --add-host=host.docker.internal:host-gateway mlforge:latest
--- src/internal/assets/assets.go
+++ src/internal/assets/assets.go
@@ -2,4 +2,6 @@ package assets
 
 import "embed"
 
+//go:embed db/ddl.sql
+//go:embed swagger/*
 var Files embed.FS
