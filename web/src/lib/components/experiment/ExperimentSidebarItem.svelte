d1b1d8ef90095e2af901a7e360ecaeb07d9488b3
Ayato Matsui <ayatomatsui010@gmail.com>
2026-08-28 09:51:50 +0000

    fixed experiment component

--- web/src/lib/components/experiment/ExperimentSidebarItem.svelte
+++ web/src/lib/components/experiment/ExperimentSidebarItem.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import type { ExperimentDetails } from "$lib/types/base/experiment";
+    import type { ExperimentSummary } from "$lib/types/base/experiment";
     import MLforgeSideBarItem from "../common/MLforgeSideBarItem.svelte";
     let {
         experiment,
         active = false,
     }: { 
-        experiment: ExperimentDetails;
+        experiment: ExperimentSummary;
         active?: boolean;
     } = $props();
 </script>
 
-<MLforgeSideBarItem path={`/${experiment.experimentSummary.id}`} active={active}>
-    (ID: {experiment.experimentSummary.id}) {experiment.experimentSummary.name}
+<MLforgeSideBarItem path={`/${experiment.id}`} active={active}>
+    (ID: {experiment.id}) {experiment.name}
 </MLforgeSideBarItem>
