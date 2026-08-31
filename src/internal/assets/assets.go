c54538eb07360669551281fc00dc895ab3bc4729
Ayato Matsui <ayatomatsui010@gmail.com>
2026-08-31 08:32:18 +0000

    fixed missing web connection

--- src/internal/assets/assets.go
+++ src/internal/assets/assets.go
@@ -5,4 +5,7 @@ import "embed"
 //go:embed db/ddl.sql
 //go:embed swagger/*
 //go:embed swagger/node_modules/swagger-ui-dist/*
+//go:embed web/*
+//go:embed web/_app/*
+//go:embed web/_app/**/*
 var Files embed.FS
